c26d8dc18e9ed3da52674c533ee8b9b7eebacc2a
Ubuntu <mcpenny@LinuxSandbox2.a3mzdzaqk55uxkucoumzfxerhd.lx.internal.cloudapp.net>
2019-09-11 14:56:29 +0900

    Commiting from Linux_Sandox

---
+++ NodeJS8_TRW:PS-SJSTS/read-stream.js
@@ -0,0 +1,3 @@
+'use strict' 
+require('fs').createReadStream(process.argv[2]).on('data', chunk => process.stdout.write(chunk)).on('error', err => process.stderr.write(`ERROR: ${err.message] \n`));
+
